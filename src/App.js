71a3d1f9eed641cc856c521e98844bd2262e3f8a
jubakitiashvili <juba.kitiashvili@gmail.com>
2020-07-27 01:07:43 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/TodoList.js';
+import './components/TodoInput.js';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput.js';
+import TodoList from './components/TodoList.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoInput />
+      <TodoList />
     </div>
   );
 }
